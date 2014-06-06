a470556c6efd353d82730ecd627a0bb30178ff7e
DukeW <dukew@edu-soft.eu>
2014-06-06 18:03:17 +0300

    Fixed Edit

--- src/edit.php
+++ src/edit.php
@@ -1,7 +1,7 @@
 <?php
 include "config.php";
 $id=$_GET["id"];
-$text=urlencode($_GET["text"]);
+$text=urlencode($_GET["text"]); 
 $query="UPDATE  `bugster`.`bug` SET  `text` =  '$text' WHERE  `bug`.`ID` =$id;";
 mysql_query($query);
 ?>
